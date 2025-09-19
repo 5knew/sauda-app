8693643b599e41bd460b9f6c4b44f9e662ffbb58
Shynggys Muratkhan <s.muratkhan@aues.kz>
2025-09-19 16:43:34 +0500

    Initial commit

---
+++ src/test/java/com/sauda/sauda_app/SpringBootApplicationWithKeycloakIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sauda.sauda_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootApplicationWithKeycloakIntegrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
